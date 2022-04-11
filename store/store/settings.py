408f763fe49e16b88a455292d081fffa371ffbcd
Ruslan <rus.mur2001@gmail.com>
2022-04-11 15:04:24 +0600

    3 commit

--- store/store/settings.py
+++ store/store/settings.py
@@ -145,7 +145,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'rus.mur2001@gmail.com'
-EMAIL_HOST_PASSWORD = '22121974Aa'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
